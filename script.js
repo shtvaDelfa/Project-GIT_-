33374e942e9e0b6f73d306642cf545da5b777fc7
shtvaDelfa <shestakova.delfa@gmail.com>
2025-12-06 17:01:56 +0500

    это исходная первая версия проекта

---
+++ script.js
@@ -0,0 +1,62 @@
+// alert("lj,hjt enhj")
+
+// prompt("yfgtib dfi k.,bvsq irjkmysq ghtlvtn, "ytpyf." )
+
+// confirm("ds[ggjhjlbagbjhnh;j.hbwrtgkhjw")
+
+// console.log("bgfhjefglrlqfg")
+
+// let subject = "gthtvtyf"
+// console.log?("subject")
+
+// let num1 = 100;
+// let num2 = 12.5;
+
+// let res = num1 + num2
+// console.log(res)
+
+// let str1 = "ghbdtn"
+// let str2 = "gjrf"
+
+// let name = 'gtnh'
+// let str3 = `ghbdtn, ${name}`;
+// console.log(str3)
+
+// let subject = prompt("yfgtib dfi k.,bvsq irjkmysq ghtlvtn");
+// console.log(`dfi k.,bvsq ghtlven: ${subject}`)
+
+
+// console.log("dfi kvkcvkjif fvgdvfds " + subject)
+
+// let name = prompt("как вас зовут")
+// let color =  prompt("кокой ваш любимый цвет")
+// let animal = prompt("кокое ваше любимое животное")
+// alert(`итак ваше имя:${name}, ваш любимый цвет: ${color} ваше любимое животное: ${animal}`)
+
+// let str = "привет, javascript"
+// let strLen = str.length; 
+// console.log("длина строки:" + strLen)
+// let strUpper = str.toUpperCase()
+// console.log(strUpper)
+
+// let strLower = str.toLocaleLowerCase()
+// console.log(strLower)
+
+// console.log(str.includes(","))
+
+// let age;
+// console.log(age);
+
+let nameUser = "petr";
+let num = 100;
+let flag = true;
+let a = null;
+let b = undefined;
+let num2 = 5.6;
+
+console.log(typeof nameUser)
+console.log(typeof num)
+console.log(typeof num2)
+console.log(typeof flag)
+console.log(typeof b)
+console.log(typeof a)
